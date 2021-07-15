8399a3450510920e4ed2ffdcb9b0e4f16f5b0b07
Felipe Melo <felipesilvamelo28@gmail.com>
2021-07-15 14:29:41 -0300

    commit inicial

---
+++ src/main/java/com/example/testekafka/TestekafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestekafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestekafkaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/testekafka/service/KafkaExample.java
@@ -0,0 +1,80 @@
+package com.example.testekafka.service;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class KafkaExample {
+    private final String topic;
+    private final Properties props;
+
+    public KafkaExample(String brokers) {
+        this.topic = "TesteOrdenacao";
+
+        String serializer = StringSerializer.class.getName();
+        String deserializer = StringDeserializer.class.getName();
+        props = new Properties();
+        props.put("bootstrap.servers", brokers);
+        props.put("group.id", "TesteOrdenacao");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("auto.offset.reset", "earliest");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", deserializer);
+        props.put("value.deserializer", deserializer);
+        props.put("key.serializer", serializer);
+        props.put("value.serializer", serializer);
+        props.put("security.protocol", "SASL_SSL");
+        props.put("sasl.mechanism", "SCRAM-SHA-256");
+    }
+
+    public void consume() {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topic));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(1000);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("%s [%d] offset=%d, key=%s, value=\"%s\"\n",
+                        record.topic(), record.partition(),
+                        record.offset(), record.key(), record.value());
+            }
+        }
+    }
+
+    public void produce(String message) {
+        Thread one = new Thread() {
+            public void run() {
+                try {
+                    Producer<String, String> producer = new KafkaProducer<>(props);
+                    producer.send(new ProducerRecord<>(topic, null, message));
+                    System.out.println("msg enviada");
+                } catch (Exception v) {
+                    System.out.println(v);
+                }
+            }
+        };
+        one.start();
+    }
+
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9091");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+
+}
